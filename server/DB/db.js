e451fb72b175217fe604be37c2708ad0b66754bf
Yuncheng Jia <1369590972@qq.com>
2022-07-17 03:34:37 -0700

    SQL3 path change to current folder

--- server/DB/db.js
+++ server/DB/db.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-const seq = new Sequelize('sqlite:../SQL3/spot.db', { logging: false });
+const seq = new Sequelize('sqlite:./SQL3/spot.db', { logging: false });
 // const seq = new Sequelize(
 //     'sqlite:/Users/creep/Something/TryNodeJS/网页课/sSQL3/spot.db'
 // );
